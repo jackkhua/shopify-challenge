f10d7f23d093c02c675c68608fa680fb165828e5
Jack Hua <j7hua@uwaterloo.ca>
2021-05-02 13:56:43 -0400

    search functions

---
+++ util/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, url_for
+from db.image_db import mock_db
+
+app = Flask(__name__, template_folder='template')
+db_connection = mock_db()
+
+
+@app.route("/")
+def index():
+    returned_urls = db_connection.home_page()
+    return render_template('index.html', value = returned_urls)
+
+
+@app.route("/image", methods = ['POST'])
+def display():
+    image_id = int(request.form['image_id'])
+    print(image_id)
+    # image_url = request.form['image_url']
+    current_image = db_connection.data_from_id(image_id)
+    related_id = db_connection.search(current_image['category'])
+    related_id.remove(image_id)
+    related_images = db_connection.data_from_id(related_id)
+    data_set = (current_image, related_images)
+    print(data_set)
+    return render_template('display_image.html', value=data_set)
+        
+
+
+
+@app.route("/search", methods = ['POST'])
+def search():
+    key = request.form['key']
+    key = key.split(',')
+    returned_urls = db_connection.search(key)
+    related_images = db_connection.data_from_id(returned_urls)
+
+    return render_template('index.html', value =related_images)
---
+++ util/db/image_db.py
@@ -0,0 +1,100 @@
+# This mocks a database that can be hosted locally or remotely by storing images metadata in a dictionary
+import re
+
+
+class mock_db:
+    def __init__(self):
+        # This is some examples of existing images in the mock database
+        self.db = [
+            {
+                'id': 0,
+                'title': 'fruit basket',
+                'image_url': 'fruit_basket_image.jpeg',
+                'category': ['fruit', 'item']
+            },
+            {
+                'id': 1,
+                'title': 'watermelon',
+                'image_url': 'watermelon.jpeg',
+                'category': ['fruit', 'item']
+            },
+            {
+                'id': 2,
+                'title': 'league of legends festival',
+                'image_url': 'league_festival.jpeg',
+                'category': ['game', 'character']
+            },
+            {
+                'id': 3,
+                'title': 'cs go dragon lore',
+                'image_url': 'dragon_lore.jpeg',
+                'category': ['game', 'weapon']
+            },
+            {
+                'id': 4,
+                'title': 'nba 2k20',
+                'image_url': 'nba2k.jpeg',
+                'category': ['game', 'basketball', 'character', 'human']
+            },
+            {
+                'id': 5,
+                'title': 'kyrie irving',
+                'image_url': 'kyrie.jpg',
+                'category': ['basketball', 'human']
+            },
+            {
+                'id': 6,
+                'title': 'naruto',
+                'image_url': 'naruto.png',
+                'category': ['character','cartoon']
+            }
+        ]
+        # This can be another db called (image_category_detail) and stores the referrence of the image ids in json format 
+        
+        # self.category_list = ['fruit','item','game','character','weapon','basketball','human','cartoon']
+        self.category_db = {
+            'fruit': [0, 1],
+            'item': [0, 1],
+            'game': [2,3,4],
+            'character': [2, 4, 6],
+            'weapon': [3],
+            'basketball': [4, 5],
+            'human': [4, 5],
+            'cartoon': [6],
+        }
+
+    def search(self, keys: list) -> list:
+        result_images = []
+        # if the key provided by the user is a key in category_db, it can speed up the runtime
+        for key in keys:
+            key = key.strip()
+            if key in self.category_db:
+                for image in self.category_db[key]:
+                    if image not in result_images:
+                        result_images.append(image)
+            # Then check the key is a title, this process takes O(n) time, alternative way would be storing all the titles in another
+            # db but if space is costy we can use this method
+            else:
+                for image in self.db:
+                    # not breaking from here because there's possibility for two images having the same title
+                    if re.search(key, image['title']):
+                        result_images.append(image['id'])
+        return result_images
+    
+    def data_from_id(self, image_id):
+        if isinstance(image_id, int):
+            return self.db[image_id]
+        else:
+            return [self.db[image] for image in image_id]
+    
+    
+    def home_page(self):
+        # result_images = []
+        # for image in self.db:
+        #     result_images.append((image['image_url'], image['category']))
+        return self.db
+
+
+# if __name__ == "__main__":
+    # test = mock_db()
+    # print(test.search('human'))
